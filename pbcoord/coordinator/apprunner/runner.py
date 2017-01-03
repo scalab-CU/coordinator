993c3b3c907869b46aa900d8c0fb5c35c3b5cdb7
Xizhgou Feng <xizhou.feng@gmail.com>
2017-01-02 22:53:27 -0500

    implement a simple runner

--- pbcoord/coordinator/apprunner/runner.py
+++ pbcoord/coordinator/apprunner/runner.py
@@ -6,11 +6,23 @@ Created on Jan 2, 2017
 import argparse
 import json
 import pprint
+import subprocess
 
 def parseJsonCfg(jsonCfg):
     with open(jsonCfg) as cfgFile:
         data = json.load(cfgFile)
     return data
+
+def runApp(appCfg, rscCfg):
+    script = "{}.job".format(appCfg["app"])
+    with open(script, "w") as f:
+        f.write("#!/bin/bash\n\n")
+        f.write("#PBS -l nodes={}\n\n".format(rscCfg["hostname"]))
+        f.write("export OMP_NUM_THREADS={}\n".format(rscCfg["num_cores"]))
+        f.write("{}\n".format(appCfg["path"]))
+    
+    subprocess.call(["qsub", script])
+    
 
 if __name__ == '__main__':
     p = argparse.ArgumentParser(description="run application under power bound")
@@ -23,6 +35,8 @@ if __name__ == '__main__':
     rscCfg = parseJsonCfg(args.resource_cfg)
     pprint.pprint(appCfg)
     pprint.pprint(rscCfg)
+    
+    runApp(appCfg, rscCfg)
     
     
     
